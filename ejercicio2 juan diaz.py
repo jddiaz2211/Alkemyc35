8c88ca236ea0920fb1b39121a288975040b34a9e
jddiaz2211 <119127262+jddiaz2211@users.noreply.github.com>
2024-02-22 20:39:29 -0300

    Add files via upload

---
+++ ejercicio2 juan diaz.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self, cantidad_patas):
+        self.cantidad_patas = cantidad_patas
+    
+    def comer(self):
+        return "Estoy comiendo"
+
+class Perro(Animal):
+    def __init__(self, cantidad_patas, nombre, raza):
+        super().__init__(cantidad_patas)
+        self.nombre = nombre
+        self.raza = raza
+    
+    def correr(self):
+        return "Estoy corriendo"
+
+class Aguila(Animal):
+    def volar(self):
+        return "Estoy volando"
+
+
+perro = Perro(4, "Bobby", "Labrador")
+print(perro.comer())
+print (f"""{perro.nombre} ya comio""")
+print (".........")
+print(perro.correr())
+print (f"""{perro.nombre} estaba corriendo""")
+print (".........")
+
+
+aguila = Aguila(2)
+print(aguila.comer())
+print (".........")
+print(aguila.volar())
